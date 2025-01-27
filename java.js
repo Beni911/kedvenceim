8ab36da6e9ac0b253af6e2bca59ceb6e43502d9b
Viszló Benjámin <beniforspam2@gmail.com>
2025-01-27 21:20:58 +0100

    finomitas-alpha_1.2

--- java.js
+++ java.js
@@ -9,3 +9,10 @@ torles.addEventListener('click', () => {
     cimek.classList.toggle('active');
     offscreenmenu.classList.toggle('active');
 });
+
+let scrollSpeed = 15;
+
+window.addEventListener('wheel', (event) => {
+    event.preventDefault();
+    window.scrollBy(0, event.deltaY * scrollSpeed / 50);
+}, { passive: false });
